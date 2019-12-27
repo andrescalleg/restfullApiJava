84b904cd9ea09a319e566cd99edea52cb2f21a69
andrescallepayu <andres.calle@payulatam.com>
2019-12-27 16:26:44 -0500

    Add parameter to controller

--- src/main/java/com/zemoga/challenge/controller/MainController.java
+++ src/main/java/com/zemoga/challenge/controller/MainController.java
@@ -7,7 +7,9 @@ import com.zemoga.challenge.repository.PortfolioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
@@ -25,8 +27,8 @@ public class MainController {
         }
     };
 
-    @GetMapping("/")
-    public String welcome(Model model){
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    public String welcome(@PathVariable("name") String name, Model model){
         List tweets = new ArrayList();
         tweets.add("Primer cometario");
         tweets.add("segundo cometario");
